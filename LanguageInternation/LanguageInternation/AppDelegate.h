77b02b52568b142b7c08059bc2cadaff6752ac38
shunzhitang <2311901596>
2015-12-28 15:01:54 +0800

    initApp

---
+++ LanguageInternation/LanguageInternation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LanguageInternation
+//
+//  Created by tang on 15/12/28.
+//  Copyright © 2015年 shunzhitang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LanguageInternation/LanguageInternation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LanguageInternation
+//
+//  Created by tang on 15/12/28.
+//  Copyright © 2015年 shunzhitang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
